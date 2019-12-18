a058a2709ff3784dc4cba99379196193d4719074
Yesterday17 <t@yesterday17.cn>
2019-12-18 11:57:47 +0800

    Project structure.

---
+++ src/options/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>PUG Settings</h1>
+    <p>You can adjust option below.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 20px;
+}
+</style>
